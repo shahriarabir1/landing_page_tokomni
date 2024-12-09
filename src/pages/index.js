42e18925e947d2773e60c2cb222be8615f310a00
shahriarabir <shahriarabirjob@gmail.com>
2024-12-09 18:49:24 +0600

    static

--- src/pages/index.js
+++ src/pages/index.js
@@ -139,7 +139,7 @@ export default function Home() {
       <VisionMissionSection/>
       <ConnectSection/>
       <TestimonialCarousel/>
-      <Recognitions/>
+    
       <Footer/>
     </header>
   );
